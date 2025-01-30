a8f19b3ff5daea397fadebf24c5f8c2d7c13735b
Wolf Biter <mj06974@st.habib.edu.pk>
2025-01-29 20:07:23 -0600

    removed https and added http

--- src/app/CVCustomizer/page.tsx
+++ src/app/CVCustomizer/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
   const [pdfFile,setpdfFile]=useState('')
 async function sendJobDescription(jobDescription:string){
     try{
-        axios.post('https://tjbravo.com:8000/description',{
+        axios.post('http://tjbravo.com:8000/description',{
             description:jobDescription
         })
     }
@@ -24,7 +24,7 @@ async function sendJobDescription(jobDescription:string){
 async function getCustomCV() {
     try {
       // Replace with the correct API URL
-      const response = await axios.get("https://tjbravo.com:8000/customCV", {
+      const response = await axios.get("http://tjbravo.com:8000/customCV", {
         responseType: "blob", // To handle the binary response
       });
   
