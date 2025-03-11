c140318130395978701862d4bd7081aef41b86e9
Talha771 <talha.j771@gmail.com>
2025-03-11 02:16:24 -0500

    intersectionObserver Mastian

--- src/app/components/About/About.tsx
+++ src/app/components/About/About.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 import styles from "./About.module.scss";
 import Image from "next/image";
+import { useIntersectionObserver } from "@/app/lib/hooks/useIntersectionObserver";
 
 const About = () => {
+  const intersectionObserverFunc = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => {
+      let element = entry.target as HTMLElement;
+      element.style.transition = "transform 0.5s ease, opacity 0.5s ease";
+
+      if (entry.isIntersecting) {
+        element.style.transform = "scale(1)";
+        element.style.opacity = "1";
+      } else {
+        element.style.transform = "scale(0.8)";
+        element.style.opacity = "0";
+      }
+    });
+  };
+  const useEffectCB = () => {
+    // create an intersection observer in here, and then use it to return the class of the div intersection
+    console.log("I have been called");
+    const observer = new IntersectionObserver(intersectionObserverFunc, {
+      threshold: 0.5,
+    });
+    let thingsObserved = document.querySelector(`.${styles.About}`);
+    if (thingsObserved) {
+      observer.observe(thingsObserved);
+    }
+  };
+  useEffect(useEffectCB, []);
   return (
     <div className={styles.About}>
       <div className={styles.pfp}>
